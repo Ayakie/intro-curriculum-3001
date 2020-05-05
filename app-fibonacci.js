0e8a8027b6da78e3cdb67ebc7b70f142080e5cbb
AkieNakai <ayalake156@gmail.com>
2020-05-05 13:48:43 +0900

    配列によるアルゴリズムの改善

--- app-fibonacci.js
+++ app-fibonacci.js
@@ -1,13 +1,27 @@
 'use strict';
-function fib(n) {
-    if ((n==0) || (n==1)) {
-        return n;
-    } else {
-        return fib(n-1) + fib(n-2)
-    }
-}
-const length = 40;
 
-for (let i = 0; i <= length; i++) {
-    console.log(fib(i)) 
+function fib(n) {
+    const list = [];
+
+    if (n==0) {
+        list.push(0);
+    } else if (n==1) {
+        list.push(0);
+        list.push(1);
+    } else {
+        list.push(0);
+        list.push(1);
+        for (let i=2; i<=n; i++) {
+            var val = list[i-1] + list[i-2];
+            list.push(val);
+            // console.log(list[i])
+        }
+    }
+    return list
+}
+
+const length = 40;
+var ans = fib(length)
+for (let i=0; i < ans.length; i++) {
+    console.log(ans[i])
 }
